9e0a6e9522b9f83bd574ddd9d04358ad313ff6e2
Nader Frigui <nadfri@gmail.com>
2025-04-17 13:21:20 +0200

    Fix: replaceSWS

--- src/utils/replaceSWS.ts
+++ src/utils/replaceSWS.ts
@@ -1,5 +1,8 @@
 export function replaceSWS(text: string): string {
-  return text
-    .replace(/\bsws\b/g, "ﷺ")
-    .replace(/\(que la prière d['']Allah et Son salut soient sur lui\)/g, "ﷺ");
+  return (
+    text
+      .replace(/\bsws\b/g, "ﷺ")
+      // Updated regex to match both ' and ’
+      .replace(/\(que la prière d['’]Allah et Son salut soient sur lui\)/g, "ﷺ")
+  );
 }
