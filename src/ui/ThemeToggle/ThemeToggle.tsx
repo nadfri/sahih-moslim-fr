866f6e23caa098d8cca1d353d7a4d84fff14e599
Nader Frigui <nadfri@gmail.com>
2025-05-09 18:04:05 +0200

    Fix: ThemeToggle ssr

--- src/ui/ThemeToggle/ThemeToggle.tsx
+++ src/ui/ThemeToggle/ThemeToggle.tsx
@@ -4,13 +4,19 @@ import { useLayoutEffect, useState } from "react";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 export default function ThemeToggle() {
-  const initialTheme = localStorage.getItem("theme") || "dark";
-
-  const [theme, setTheme] = useState(initialTheme);
+  // Only access localStorage in useLayoutEffect to avoid SSR issues
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
   useLayoutEffect(() => {
-    //useLayoutEffect prevent flash of light theme
+    const stored = localStorage.getItem("theme");
+
+    if (stored === "light" || stored === "dark") setTheme(stored || "dark");
+  }, []);
+
+  useLayoutEffect(() => {
+    // Prevent flash of light theme
     document.body.setAttribute("data-theme", theme);
+
     localStorage.setItem("theme", theme);
   }, [theme]);
 
@@ -20,14 +26,14 @@ export default function ThemeToggle() {
 
   return (
     <button
-      className="flex items-center justify-center rounded-full transition-colors duration-300 hover:bg-gray-300  p-1"
+      className="flex items-center justify-center rounded-full transition-colors duration-300 hover:bg-gray-300 dark:hover:bg-gray-700 p-1"
       onClick={toggleTheme}
       aria-label={`Toggle theme to ${theme === "light" ? "dark" : "light"}`}
     >
       {theme === "light" ? (
         <SunIcon className="size-6 text-yellow-400" />
       ) : (
-        <MoonIcon className="size-6 text-white/80" />
+        <MoonIcon className="size-6 text-black/80 dark:text-yellow-200" />
       )}
     </button>
   );
